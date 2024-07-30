87bb286608efe50bb36a62e8af892ea414bd0a6a
Kevin420 <155149448+aliyosakevin428@users.noreply.github.com>
2024-07-30 21:49:35 +0800

    ubah sedikit footernya

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return(
     <footer className="footer footer-center bg-base-300 text-base-content p-5">
   <aside>
-    <p className="text-sm">Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+    <p className="text-sm">Copyright © {new Date().getFullYear()} - All right reserved by S.H.I.E.L.D Ltd</p>
   </aside>
 </footer>
   );
